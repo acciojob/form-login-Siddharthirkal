0ad69d29ed06d36f8842c0f9bee5dd4e94434bd5
Siddharthirkal <104129670+Siddharthirkal@users.noreply.github.com>
2026-01-23 00:11:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 function getFormvalue() {
     //Write your code here
+	const firstName=document.forms["form1"]["fname"].value;
+	const lastName=document.forms["form1"]["lname"].value;
 
+	alert(firstName+" " + lastName);
 }
